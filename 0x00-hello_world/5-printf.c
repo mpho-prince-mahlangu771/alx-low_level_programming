45e9ae089fefb8444e7bf3eab4e8451abebef769
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-02 14:46:38 -0700

    adding a c program that prints a string on the std-output using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: displays text on the standard-output(screen) using printf
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return 0;
+}
