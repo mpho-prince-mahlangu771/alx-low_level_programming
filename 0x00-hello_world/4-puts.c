be31b9c3f7e7ae7e6cbc40b6f735f05d29378c11
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-02 14:02:26 -0700

    adding s c program that outputs text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * description: prints output to the screen using puts()
+ *
+ * return: 0
+ */
+int main(void)
+{
+	char mystring[] = " Programming is like building a multilingual puzzle";
+	puts(mystring);
+	return (0);
+}
