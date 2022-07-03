a6d60d3bac262a2c023e57979a8f8dc3cfbba241
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-02 22:46:20 -0700

    adding file: 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: this program outputs the size of various C data  types
+ * to compile use: gcc filename.c -m32/m64 -o size32/size64 2> destination dir
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s) \n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
