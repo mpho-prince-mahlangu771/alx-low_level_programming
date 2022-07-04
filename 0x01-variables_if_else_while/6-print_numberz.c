d6fb45f0802326816199dd4c977b22da3d010cc7
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-04 13:09:59 -0700

    adding file: 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints single digit # using only putchar
+ * the digits are represented using ASCII codes
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
