5b84c35ac0bc363a042571e42f32fc727ca6f820
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-04 06:34:39 -0700

    adding file: 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digits # starting at 0
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
+			
