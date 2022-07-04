44c1af514292267e7fc5a9f2c6c75218789021ad
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-04 15:50:30 -0700

    adding file: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints a quote to the standard error
+ *
+ * Return: Always 1 (error)
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora "
+		       "Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
+
