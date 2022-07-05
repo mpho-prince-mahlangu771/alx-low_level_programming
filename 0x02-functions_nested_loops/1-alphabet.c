886b389dcecbada71962f14404189cf28aa39d3c
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-05 06:22:20 -0700

    adding files: 1-main.c and 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: prints the alphabet in lowercase using _putchar()
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char lower_alphabet;
+
+	for (lower_alphabet = 'a'; lower_alphabet <= 'z'; lower_alphabet++)
+		_putchar(lower_alphabet);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
