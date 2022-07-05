fab317542767db38e343445668b75801f17c6e5f
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-05 05:57:48 -0700

    adding files: main.h, _putchar.c, and 0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,7 @@
 #include <unistd.h>
 /**
- * main -Entry point
  *
- * Description: 
- *
- * Return: on success 1
- * on error -1
+ * Description: contains all the function prototypes for the - 
+ * 0x02-functions_nested_loop directory
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
