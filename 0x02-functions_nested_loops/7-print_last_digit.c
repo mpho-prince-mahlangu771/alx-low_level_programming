1a8315a6264549e560088a4f1e64952cff696d43
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 12:32:39 -0700

    adding file: 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit: - prints the last digit of a number
+ *@n: input from 7-main.c
+ * Return: last digit of a n
+ */
+int print_last_digit(int n)
+{
+	unsigned int last_digit;
+
+	last_digit = n % 10;
+	return (last_digit);
+}
