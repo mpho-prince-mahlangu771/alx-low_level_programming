230f2992efaa57eac9fa04d8ddb56d1847681650
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 06:22:26 -0700

    adding files: 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 /**
  * int _isalpha - Checks if c is alphabet
+ * @c: c is input
  *
- * Description: This function accepts input and perform
- * checks to determine if the input is an alphabet or not
+ * Description: This function checks if input is an alphabet or not
  *
- * Return: 1 if is alphabet and 0 if not
+ * Return: Returns  1 if is alphabet and 0 if not
  */
 int _isalpha(int c)
 {
