8c150df189e9615327fa8b1cfecd86beea20b3d5
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 15:44:45 -0700

    updating  file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * print_to_98 - Prints all natural #'s from n to 98
- * @n: n is a natural number 
+ * @n: Input n is a natural number 
  * Return: Natural numbers
  */
 void print_to_98(int n)
