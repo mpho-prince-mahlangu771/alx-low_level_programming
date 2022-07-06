2d41ae27a507130e153a84394b6717d645121209
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 15:07:11 -0700

    ading files: 9-main.c and 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table,starting at 0
+ * Return: time_table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int multiplication;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			multiplication = i *j;
+			if (multiplication <= 9)
+				_putchar(' ');
+			else
+				_putchar((multiplication / 10) + '0');
+			_putchar((multiplication % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
