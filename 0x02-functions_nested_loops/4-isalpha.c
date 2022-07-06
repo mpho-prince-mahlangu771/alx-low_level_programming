387814b41560c342721e7cec1d12190b8fb1bd6f
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 06:04:46 -0700

    adding files: main.h and 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * int _isalpha - My custom made alphabet checker similar to man isalpha()
- * @c: Is the input program checks
+ * int _isalpha - Checks if c is alphabet
+ * @c: Input for function
  *
- * Description: Checks if a given input is an alphabet or not
+ * Description: This function accepts input and perform
+ * checks to determine if the input is an alphabet or not
+ *
  * Return: 1 if is alphabet and 0 if not
  */
 int _isalpha(int c)
