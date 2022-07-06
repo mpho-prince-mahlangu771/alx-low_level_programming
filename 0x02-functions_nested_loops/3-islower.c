6efec1b3c4ab1a3a6dfca0181e9d1ca8374299fe
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 03:50:28 -0700

    adding file: 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,6 @@
  * @c: Is a character provided in main.c
  * 
  * Description: checks whether int c is a lowercase character using ascii
- *
  * Return: 1 if c is lowercase, 0 if not
  */
 int _islower(int c)
