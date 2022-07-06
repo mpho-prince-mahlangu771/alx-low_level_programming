6d8400ac18abd404e7382319999b73a56f4658fd
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 13:12:24 -0700

    adding files: 10-main.c and 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add: adds two intergers
+ * @a: first integer input
+ * @b: second integer input
+ * Return: sum of two integers
+ */
+int add(int a, int b) 
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
