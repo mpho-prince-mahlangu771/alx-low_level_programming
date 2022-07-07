7e6317bf7c81cc78f480e61c99390154a86d5478
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 20:45:14 -0700

    adding files: 6-main.c and 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return(-n);
+}
+
+
