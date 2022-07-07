cfad445db3245d8de5e13f0d32dd86efabcddefa
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-06 22:41:13 -0700

    adding header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#include <unistd.h>
+/**
+ *
+ * Description: contains all the function prototypes for the - 
+ * 0x04-more_functions_nested_loop directory
+ */
+int _putchar(char c);
+int _isupper(int c);
