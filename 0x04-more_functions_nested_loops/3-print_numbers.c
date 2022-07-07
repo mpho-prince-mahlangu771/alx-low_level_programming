b38cecca503cd3b5fa9567eb95c4270758475e8c
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-07 13:06:25 -0700

    adding new files

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers 0 to 9
+ * Return: Numbers
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
