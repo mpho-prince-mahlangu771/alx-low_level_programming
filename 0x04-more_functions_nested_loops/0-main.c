8e544c2674e843ab91a6f1b1544ec51ad033e032
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-07 11:43:50 -0700

    adding files 0-main.c and 0-isupper.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -2,6 +2,7 @@
 /**
  * main -Checks the code
  *
+ * Description: the '0'character is required for _putchar to function properly
  * Return: Always 0
  */
 int main(void)
@@ -10,16 +11,19 @@ int main(void)
 
        	_putchar('A');
 	_putchar(':');
+	_putchar(' ');
 	c = _isupper('A');
 	_putchar(c + '0');
 	_putchar('\n');
 	_putchar('a');
 	_putchar(':');
+	_putchar(' ');
 	c = _isupper('a');
 	_putchar(c + '0');
 	_putchar('\n');
 	_putchar(']');
 	_putchar(':');
+	_putchar(' ');
 	c = _isupper(']');
 	_putchar(c + '0');
 	_putchar('\n');
