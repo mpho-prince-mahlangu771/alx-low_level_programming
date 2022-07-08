cdbd94da6f8430dfdc615eb7b82e5b615a545643
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-08 06:48:06 -0700

    adding file: 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Checks the code
+ * Description: Prints the #'s from 1 to 100
+ * for multiples of 3 print Fizz instead of the number
+ * for multiples of 5 print Buzz insteadd of the number
+ * for multiples of both 3 and 5 print the word FizzBuzz
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
