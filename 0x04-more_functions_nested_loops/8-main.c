0d9d13a97019819fc5325b9ec5249802e80806c5
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-08 05:27:51 -0700

    adding test file: 8-main.c, for the 8-print_square.c file

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Checks the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_square(2);
+	print_square(4);
+	print_square(10);
+	print_square(0);
+	print_square(-5);
+	return (0);
+}
