b4294198ab74d318fdff215e8d4b4fe5918a6a98
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-11 05:27:56 -0700

    updated file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * swap_int - Swaps values of two integers
- * @*a: Pointer variable pointing to address of int a
- * @*b: Pointer variable pointing to address of int b
+ * @a: Pointer variable pointing to address of int a
+ * @b: Pointer variable pointing to address of int b
  * Description: Swaps int a and b using pointers a and b containing address
  * Return: void
  */
