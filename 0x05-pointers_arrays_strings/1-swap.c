4f8a29e9dd72bdeaf3a18c535e219a479fb4420d
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-11 05:24:39 -0700

    updated file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,7 @@
  * swap_int - Swaps values of two integers
  * @*a: Pointer variable pointing to address of int a
  * @*b: Pointer variable pointing to address of int b
+ * Description: Swaps int a and b using pointers a and b containing address
  * Return: void
  */
 void swap_int(int *a, int *b)
