703982febe38bf90e667cc031b3d96698bc72811
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-11 13:42:38 -0700

    added files: 4-main.c and 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - checks the code: 4-print_rev.c
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers";
+	print_rev(str);
+	return (0);
+}
