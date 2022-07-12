1cfc1586b38549b56b0670ad06f81ee64295d221
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-12 09:05:07 -0700

    updated file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,12 @@
 void puts2(char *str)
 {
 	int i;
-	int strlen;
+	int len;
 
-	strlen = _strlen(str);
-	for (i = 0; i < strlen; i += 2)
+	for (len = 0; str[len] != '\0'; ++len)
+	{
+	}
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
