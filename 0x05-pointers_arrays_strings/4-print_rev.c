7ccfe4101234d9b6c43365627bafe799ccc109bf
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-11 21:24:31 -0700

    updated file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,7 @@
 /**
  * print_rev - Prints a string in reverse
  * @s: A pointer string. A string is an array & pointer
+ * @strlen: calculates the length of string S
  * Return: A reverse string
  */
 void print_rev(char *s)
@@ -9,7 +10,8 @@ void print_rev(char *s)
 	int strlen;
 	int i;
 
-	for (strlen = 0; s[strlen] != '\0'; ++strlen);
+	for (strlen = 0; s[strlen] != '\0';
+		       	++strlen);
 
 	for (i = strlen -1; i >= 0; i--)
 		_putchar(s[i]);
