b6aeeab7f1499e8e3e75e32ddbdde9a75785880b
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-11 21:28:05 -0700

    updated file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	int i;
 
 	for (strlen = 0; s[strlen] != '\0';
-		       	++strlen);
+	    ++strlen);
 
-	for (i = strlen -1; i >= 0; i--)
+	for (i = strlen - 1; i >= 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
