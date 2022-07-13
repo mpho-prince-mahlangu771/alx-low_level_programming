3f8cde71c0d471f2d2e9e42a5d327b425b638aa9
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-13 14:24:34 -0700

    added files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase
+ * @str: Character string 
+ * Return: Uppercase letters in string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a') && (str[i] <= 'z'))
+			str[i] = str[i] -32;
+	}
+	return (str);
+}
