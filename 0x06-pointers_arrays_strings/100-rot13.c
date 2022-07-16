8623486b2bf740d92db3bca986a062deceaf1ea2
mpho-prince-mahlangu771 <mphomahlangu771@gmail.com>
2022-07-15 20:43:35 -0700

    added file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *rot13 - Encodes a string using rot13
+ * @str: String input
+ * Return: encoded string
+ */
+char *rot13(char *str)
+{
+	int i;
+	int x;
+	int r;
+	char swap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P', 'c',
+		'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f', 
+		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i',
+		'v', 'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l',
+		'y', 'M', 'Z', 'm', 'z'};
+
+	i = 0;
+	r =13;
+	while(str[i] != '\0')
+	{
+		for (x =0; x <= 51; x++)
+		{
+			if (str[i] == swap[x])
+			{
+				str[i] = str[i] + r;
+				x = 51;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
